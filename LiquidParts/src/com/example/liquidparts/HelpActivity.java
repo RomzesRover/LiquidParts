f0e0d66141dae766b59a9a8316ea2e4160a0f730
RomzesRover <romzesrover@gmail.com>
2013-07-23 14:47:04 +0600

    first commit

---
+++ LiquidParts/src/com/example/liquidparts/HelpActivity.java
@@ -0,0 +1,64 @@
+package com.example.liquidparts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+
+import com.astuetz.viewpager.extensions.PagerSlidingTabStrip;
+
+public class HelpActivity extends FragmentActivity {
+
+	private PagerSlidingTabStrip tabs;
+	private ViewPager pager;
+	private MyPagerAdapter adapter;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    getActionBar().setTitle(getResources().getString(R.string.help));
+		tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+		pager = (ViewPager) findViewById(R.id.pager);
+		adapter = new MyPagerAdapter(getSupportFragmentManager());
+		pager.setAdapter(adapter);
+
+		final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+				.getDisplayMetrics());
+		pager.setPageMargin(pageMargin);
+
+		tabs.setViewPager(pager);
+	    // TODO Auto-generated method stub
+	}
+	
+	public class MyPagerAdapter extends FragmentPagerAdapter {
+
+		private final String[] TITLES = getResources().getStringArray(R.array.help);
+
+		public MyPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return TITLES[position];
+		}
+
+		@Override
+		public int getCount() {
+			return TITLES.length;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return new Help(getResources().getStringArray(R.array.info)[position]);
+			
+		}
+
+	}
+
+}
